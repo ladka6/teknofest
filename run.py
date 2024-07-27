2de1a2316a53b6703b583d2a607a8cab51d8593c
Ege Erdal <ege-erdal101@hotmail.com>
2024-07-27 23:38:53 +0300

    remove unnecessary parts

--- run.py
+++ run.py
@@ -8,14 +8,9 @@ embed_model = Embeddings()
 
 
 def main(message, history):
-    # embedding bul -> bu embeddinglerden schemayı dön
-    # schemayı ve queryi sqlcoder e ver
-    # sql kodunu dön
     en_message = pipe(message)
     en_message = en_message[0]["translation_text"]
-    print("User message: ", en_message)
     schema = embed_model.get_relevant_schema(en_message)
-    print(schema)
     return str(schema)
 
 
