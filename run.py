c94da58312537ed628264f99d2f85bd4e7ae0cd6
Ege Erdal <ege-erdal101@hotmail.com>
2024-08-08 17:20:34 +0300

    add sql fetching

--- run.py
+++ run.py
@@ -6,6 +6,8 @@ from teknofest.scripts import get_relevent_schema
 from teknofest.main.language_model import LanguageModel
 import os
 from dotenv import load_dotenv
+from sqlalchemy import create_engine, text
+from sqlalchemy.exc import SQLAlchemyError
 
 load_dotenv()
 
@@ -17,6 +19,18 @@ embed_model = Embeddings()
 model = LanguageModel()
 
 
+def run_query(query: str):
+    db_uri = os.getenv("DB_URI") or ""
+    engine = create_engine(db_uri)
+    try:
+        with engine.connect() as connection:
+            result = connection.execute(text(query))
+            return result.fetchall()
+    except SQLAlchemyError as e:
+        print(f"An error occurred: {e}")
+        return ""
+
+
 def main(message, history):
     en_message = pipe(message)
     en_message = en_message[0]["translation_text"]
@@ -26,7 +40,11 @@ def main(message, history):
         db_schema.append(table["name"])
     db_metadata = get_relevent_schema.get_relevant_schemas(db_schema)
     sql_query = model.run_inference(en_message, db_metadata)
-    return sql_query
+    sql_data = run_query(sql_query)
+    print(sql_data)
+    print()
+    print(sql_query)
+    return f"{sql_query} \n {sql_data}"
 
 
 demo = gr.ChatInterface(main)
